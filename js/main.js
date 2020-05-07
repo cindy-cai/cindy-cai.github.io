8ba9ab6306488bbe1a38b01a8c514195a9a7d41d
Cindy Cai <cindyycai@gmail.com>
2020-05-07 15:13:12 -0400

    new theme

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
-/* ===================================
---------------------------------------
-  NISSA - PHOTOGRAPHY STUDIO TEMPLATE
-  Version: 1.0
- -------------------------------------
- =====================================*/
+/* =================================
+------------------------------------
+	Riddle - Portfolio
+	Version: 1.0
+ ------------------------------------ 
+ ====================================*/
+
 
 
 'use strict';
 
-$(window).on('load', function() {
+
+$(window).on('load', function() { 
 	/*------------------
 		Preloder
 	--------------------*/
-	$(".loader").fadeOut();
+	$(".loader").fadeOut(); 
 	$("#preloder").delay(400).fadeOut("slow");
+
+	if($('.portfolios-area').length > 0 ) {
+		var containerEl = document.querySelector('.portfolios-area');
+		var mixer = mixitup(containerEl);
+	}
 
 });
 
+
 (function($) {
+
 	/*------------------
 		Navigation
 	--------------------*/
-	$('.nav-switch').on('click', function() {
-		$('.main-site-warp').addClass('push-side');
-		$('body').addClass('push-side');
-		setTimeout(function(){
-			hero_section();
-		}, 400)
-		
+	$('.nav-switch').on('click', function(event) {
+		$('.main-menu').slideToggle(400);
+		event.preventDefault();
 	});
-	$('.close-menu').on('click', function() {
-		$('.main-site-warp').removeClass('push-side');
-		setTimeout(function(){
-			$('body').removeClass('push-side');		
-		}, 400);
-	});
+
 
 
 	/*------------------
-		Background Set
+		Background set
 	--------------------*/
 	$('.set-bg').each(function() {
 		var bg = $(this).data('setbg');
@@ -46,110 +46,36 @@ $(window).on('load', function() {
 	});
 
 
-	/*------------------
-	   Custom Scrollbar
-	--------------------*/
-	function site_scrollbar() {
 
-		if ($(window).width() > 991) {
-			$(".main-sidebar").niceScroll({
-				cursorborder:"",
-				cursorcolor:"#f1f1f1",
-				boxzoom:false,
-				cursorwidth: 4,
-				cursorborderradius:0,
-			});
-		
-			$(".about-section, .contact-section, .elements-section").niceScroll({
-				cursorborder:"",
-				cursorcolor:"#f1f1f1",
-				boxzoom:false,
-				cursorwidth: 4,
-				cursorborderradius:0,
-			});
-		
-			$(".blog-posts").niceScroll({
-				cursorborder:"",
-				cursorcolor:"#242424",
-				boxzoom:false,
-				cursorwidth: 4,
-				autohidemode:true,
-				background: '#eeeeee',
-				railoffset: { top: 50, right: 0, left: 40, bottom: 0 },
-				railpadding: { top: 0, right: 0, left: 0, bottom: 100 },
-			});
-		
-			$(".portfolio-section").niceScroll({
-				cursorborder:"",
-				cursorcolor:"#242424",
-				boxzoom:false,
-				cursorwidth: 4,
-				autohidemode:true,
-				background: '#eeeeee',
-				railoffset: { top: 50, right: 0, left: -14, bottom: 0 },
-				railpadding: { top: 0, right: 0, left: 0, bottom: 100 },
-			});
-		}
-		
-	}
-	site_scrollbar();
-	
+	/*----------------------
+		Portfolio layout
+	------------------------*/
+	var port_fi =  $('.portfolios-area .first-item'),
+		port_si =  $('.portfolios-area .second-item'),
+		port_intro_h =  $('.portfolio-intro').innerHeight();
 
-	/*------------------
-		Video Popup
-	--------------------*/
-	$('.video-popup').magnificPopup({
-		type: 'iframe'
-	});
-
-	/*------------------
-		Hero section
-	--------------------*/
-	function hero_section() {
-		if ($(window).width() > 768) {
-			
-			var ps_w = $('.page-section').innerWidth();
-			$('.hs-item').width(ps_w / 2);
-			var trackW = (($('.hs-item').width()) * $('.hs-item').length) + ($('.hs-item').length * 50);
-			$('.hero-track').width(trackW);
-
-			var hs_width =  $(window).innerHeight() - 170;
-			$('.hs-item').height(hs_width);
-
-			$(".hero-scroll").niceScroll({
-				cursorborder:"",
-				cursorcolor:"#242424",
-				boxzoom:false,
-				cursorwidth: 4,
-				autohidemode:false,
-				background: '#eeeeee',
-				cursorborderradius:0,
-				railoffset: { top: 0, right: 50, left: 0, bottom: 0 },
-				railpadding: { top: 0, right: 0, left: 0, bottom: 0 },
-			});
-		}
-		if ($(window).width() < 768) {
-			$('.hero-track').addClass('owl-carousel')
-			$('.hero-track').owlCarousel({
-				loop: true,
-				margin: 0,
-				nav: true,
-				items: 1,
-				dots: false,
-				margin: 30,
-				navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-			});
-		}
+	if ($(window).width() > 991) {
+		port_fi.appendTo('.portfolio-intro');
+		port_si.find('.portfolio-item').height(port_intro_h + 601);
 	}
 
-	hero_section();
-
-	$(window).resize(function () { 
-		hero_section();
-		site_scrollbar();
+	$('.portfolio-item.pi-style2').each(function() {
+		var pi_width = $(this).width();
+		$(this).height(pi_width + 50);
 	});
 
-	
+
+
+	/*------------------
+		Popup
+	--------------------*/
+	$('.portfolio-item').magnificPopup({
+		type: 'image',
+		mainClass: 'img-popup-warp',
+		removalDelay: 400,
+	});
+
+
 	/*------------------
 		Accordions
 	--------------------*/
@@ -162,48 +88,41 @@ $(window).on('load', function() {
 		e.preventDefault();
 	});
 
-	/*------------------
-		Progress Bar
-	--------------------*/
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
-		}
+
+if($().circleProgress){
+
+	//Set progress circle 1
+	$("#progress1").circleProgress({
+		value: 0.75,
+		size: 146,
+		thickness: 3,
+		fill: "#979797",
+		emptyFill: "rgba(0, 0, 0, 0)"
 	});
-
-	/*------------------
-		Circle progress
-	--------------------*/
-	$('.circle-progress').each(function() {
-		var cpvalue = $(this).data("cpvalue");
-		var cpcolor = $(this).data("cpcolor");
-		var cpid 	= $(this).data("cpid");
-
-		$(this).prepend('<div class="'+ cpid +' circle-warp"></div><h2>'+ cpvalue +'<span>%</span></h2>');
-
-		if (cpvalue < 100) {
-
-			$('.' + cpid).circleProgress({
-				value: '0.' + cpvalue,
-				size: 84,
-				thickness: 5,
-				fill: cpcolor,
-				emptyFill: "rgba(0, 0, 0, 0)"
-			});
-		} else {
-			$('.' + cpid).circleProgress({
-				value: 1,
-				size: 84,
-				thickness: 5,
-				fill: cpcolor,
-				emptyFill: "rgba(0, 0, 0, 0)"
-			});
-		}
+	//Set progress circle 2
+	$("#progress2").circleProgress({
+		value: 0.83,
+		size: 146,
+		thickness: 3,
+		fill: "#979797",
+		emptyFill: "rgba(0, 0, 0, 0)"
 	});
+	//Set progress circle 3
+	$("#progress3").circleProgress({
+		value: 0.25,
+		size: 146,
+		thickness: 3,
+		fill: "#979797",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 4
+	$("#progress4").circleProgress({
+		value: 0.95,
+		size: 146,
+		thickness: 3,
+		fill: "#979797",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+}
 
 })(jQuery);
